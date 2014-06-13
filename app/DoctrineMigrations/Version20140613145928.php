086d55be9337472a1724952cc2fa94061b5df6dd
Fabian Derschatta <derschatta@users.noreply.github.com>
2014-06-13 15:02:45 +0200

    add first migration

---
+++ app/DoctrineMigrations/Version20140613145928.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20140613145928 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "postgresql", "Migration can only be executed safely on 'postgresql'.");
+        
+        $this->addSql("CREATE SEQUENCE Profiling_id_seq INCREMENT BY 1 MINVALUE 1 START 1");
+        $this->addSql("CREATE TABLE Profiling (id INT NOT NULL, timestamp TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, wall_time INT NOT NULL, cpu INT NOT NULL, memory INT NOT NULL, peak_memory INT NOT NULL, data BYTEA NOT NULL, PRIMARY KEY(id))");
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "postgresql", "Migration can only be executed safely on 'postgresql'.");
+        
+        $this->addSql("DROP SEQUENCE Profiling_id_seq CASCADE");
+        $this->addSql("DROP TABLE Profiling");
+    }
+}
