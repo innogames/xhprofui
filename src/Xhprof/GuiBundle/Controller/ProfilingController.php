70816625a41b21c06bc6f7ddd7e5f15c83a2352f
Fabian Derschatta <derschatta@users.noreply.github.com>
2014-06-27 19:35:50 +0200

    created action and view to list all profilings

--- src/Xhprof/GuiBundle/Controller/ProfilingController.php
+++ src/Xhprof/GuiBundle/Controller/ProfilingController.php
@@ -3,30 +3,59 @@
 namespace Xhprof\GuiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Xhprof\GuiBundle\Entity\ProfilingRepository;
 
 class ProfilingController extends Controller
 {
-    public function indexAction($id)
+    /**
+     * Show a single profiling in detail
+     *
+     * @param integer $id the profiling id
+     *
+     * @throws \InvalidArgumentException
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function showAction($id)
     {
+        if (!is_numeric($id)) {
+            throw new \InvalidArgumentException('Invalid id given, numeric value expected!');
+        }
         $doctrine = $this->get('doctrine');
         $profiling = $doctrine->getRepository('XhprofGuiBundle:Profiling')
             ->find($id);
         $data = null;
         if ($profiling) {
             $data = $profiling->getData();
-            /*
-            uasort($data, function($a, $b) {
-                if ($a['wt'] == $b['wt']) {
-                    return 0;
+            // sort by walltime, temporary solution
+            uasort(
+                $data,
+                function ($a, $b) {
+                    if ($a['wt'] == $b['wt']) {
+                        return 0;
+                    }
+
+                    return ($a['wt'] < $b['wt']) ? 1 : -1;
                 }
-                return ($a['wt'] < $b['wt']) ? 1 : -1;
-            });
-            */
+            );
         }
-        return $this->render('XhprofGuiBundle:Profiling:index.html.twig', array('profiling' => $profiling, 'data' => $data));
+
+        return $this->render(
+            'XhprofGuiBundle:Profiling:index.html.twig',
+            array('profiling' => $profiling, 'data' => $data)
+        );
     }
 
-    public function testAction() {
+    public function listAction()
+    {
+        $doctrine = $this->get('doctrine');
+        /** @var ProfilingRepository $repository */
+        $repository = $doctrine->getRepository('XhprofGuiBundle:Profiling');
+        $profilings = $repository->findAll();
+        return $this->render('XhprofGuiBundle:Profiling:list.html.twig', array('profilings' => $profilings));
+    }
+
+    public function testAction()
+    {
         return $this->render('XhprofGuiBundle:Profiling:test.html.twig', array());
     }
 }
