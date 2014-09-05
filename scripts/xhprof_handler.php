cf9d8374837059619a0e18194e6e60f3535089dd
Fabian Derschatta <derschatta@users.noreply.github.com>
2014-09-05 18:06:14 +0200

    prepare the xhprof handler to work as a phar archive

--- scripts/xhprof_handler.php
+++ scripts/xhprof_handler.php
@@ -1,15 +1,41 @@
 <?php
 
-use Symfony\Component\Debug\Debug;
+use Doctrine\ORM\Tools\Setup;
+use Symfony\Component\Yaml\Yaml;
+use Doctrine\ORM\EntityManager;
+use Xhprof\GuiBundle\Services\ProfilingSaveHandler;
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-Debug::enable();
+require_once 'vendor/autoload.php';
 
-require_once __DIR__.'/../app/AppKernel.php';
+$debug = false;
+if (defined('XHPROF_DEBUG')) {
+    $debug = XHPROF_DEBUG;
+}
 
-$kernel = new AppKernel('dev', true);
-$kernel->loadClassCache();
-$kernel->boot();
+$paths = array('src');
+$config = Setup::createAnnotationMetadataConfiguration(
+    $paths,
+    $debug,
+    null,
+    null,
+    false
+);
 
-$handler = $kernel->getContainer()->get('xhprof.profiling.save.handler');
+if (!defined('XHPROF_CONFIG')) {
+    throw new Exception('You need to specify a constant for the config file XHPROF_CONFIG');
+}
+if (!file_exists(XHPROF_CONFIG)) {
+    throw new RuntimeException('Config file in path ' . XHPROF_CONFIG . ' not found!');
+}
+
+$connection_parameters = Yaml::parse(file_get_contents(XHPROF_CONFIG));
+if (empty($connection_parameters['xhprof']['database'])) {
+    throw new RuntimeException('No valid database connection found, please use the standard structure!');
+}
+
+// obtaining the entity manager
+$entity_manager = EntityManager::create($connection_parameters['xhprof']['database'], $config);
+
+$handler = new ProfilingSaveHandler();
+$handler->setDoctrineEntityManager($entity_manager);
 $handler->register();
